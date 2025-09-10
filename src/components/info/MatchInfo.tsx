21d9dbf6d1e01e5a6c6f5f108d2f6dd7cf98c41c
Debadeep Sen (MSIK) <debadeep.sen@gmail.com>
2025-09-10 23:26:19 +0530

    colors WIP

--- src/components/info/MatchInfo.tsx
+++ src/components/info/MatchInfo.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { TEAMS_COLORS } from '@/constants'
 import { MatchInfoProps, MatchSummary } from '@/types'
 import { formatDate } from '@/utils/lib'
 
@@ -24,14 +25,15 @@ export const MatchInfo = ({
   }
 
   return (
-    <div className='w-full rounded-lg shadow-sm overflow-hidden mb-6'>
+    <div className='w-full overflow-hidden mb-6'>
       {/* Header */}
       <div className='px-6 py-4'>
         <h1 className='text-4xl font-bold mb-4'>
-          {info.teams[0]} <span className='font-normal'>vs</span>{' '}
-          {info.teams[1]}
+          <span style={{ color: TEAMS_COLORS[0] }}>{info.teams[0]}</span>{' '}
+          <span className='font-normal'>vs</span>{' '}
+          <span style={{ color: TEAMS_COLORS[1] }}>{info.teams[1]}</span>
         </h1>
-        <p className='text-sm mt-1'>
+        <p className='text-xs mt-1'>
           {info.matchType} • {info.venue}
           {info.city ? `, ${info.city}` : ''} •{' '}
           {info.dates.map(formatDate).join(', ')}
